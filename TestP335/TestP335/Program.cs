529473139cccf3bddfd221dc682423fe36063843
unknown <aslanrh@code.edu.az>
2023-07-28 14:21:42 +0400

    Initial Commit

---
+++ TestP335/TestP335/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq.Expressions;
+using System;
+
+namespace TestP335
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple string concatenation
+            Console.WriteLine("Hello" + " " + "String " + "!");
+
+            string firstName = "Mahesh";
+            string lastName = "Chand";
+
+            string name = firstName + " " + lastName;
+            Console.WriteLine(name);
+
+            // String Interpolation
+            string author = "Mahesh Chand";
+            string book = "C# Programming";
+           
+            string bookAuthor = $"{author} is the author of {book}.";
+            Console.WriteLine(bookAuthor);
+
+            //............................if..................................//
+
+            //int number = 2;
+            //if (number < 5)
+            //{
+            //    Console.WriteLine(number+" is less than 5" );
+            //}
+
+
+
+
+            int number = 12;
+
+            if (number < 5)
+            {
+                Console.WriteLine(number+ " is less than 5");
+            }
+            else
+            {
+                Console.WriteLine(number + " is greater than or equal to 5");
+            }
+
+
+        }
+    }
+}
