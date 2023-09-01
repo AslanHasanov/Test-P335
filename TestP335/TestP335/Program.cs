3b65997845adcfa07b58c94eeef3f2ecfbc9e08a
Aslan Hasan <aslanrh@code.edu.az>
2023-09-01 16:08:15 +0400

    LINQ GroupBy method Lab 6

--- TestP335/TestP335/Program.cs
+++ TestP335/TestP335/Program.cs
@@ -16,48 +16,50 @@ namespace TestP335
         public NotEmailAdressException(string message) : base(message) { }
     }
 
-    public class EmailValidator
-    {
-        public bool Validate(string email)
-        {
-            if (email.Length < 10)
-            {
-                throw new ShortEmailException("Email address is short.");
-            }
 
-            if (!email.EndsWith("@gmail.com"))
-            {
-                throw new NotEmailAdressException("The email address not valid");
-            }
-
-            return true;
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            var emailValidator = new EmailValidator();
-
-            try
+            var users = new List<User>()
             {
-                bool isValid = emailValidator.Validate("ali.aliyev@gmail.com");
-                Console.WriteLine(isValid);
-            }
-            catch (ShortEmailException ex)
-            {
-                Console.WriteLine($"ShortEmailException: {ex.Message}");
-            }
-            catch (NotEmailAdressException ex)
-            {
-                Console.WriteLine($"NotEmailAdressException: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
-            }
+                new User { Name = "John Doe", Age = 42, Country = "USA" },
+                new User { Name = "Jane Doe", Age = 38, Country = "USA" },
+                new User { Name = "Joe Doe", Age = 19, Country = "Germany" },
+                new User { Name = "Jenna Doe", Age = 19, Country = "Germany" },
+                new User { Name = "James Doe", Age = 8, Country = "USA" },
+            };
 
 
+            var usersGroupedByCountry = users.GroupBy(user => user.Country);
+
+            foreach (var group in usersGroupedByCountry)
+            {
+                Console.WriteLine($"Users from {group.Key} :");
+
+                foreach (var user in group) Console.WriteLine("* " + user.Name);
+            }
+
+            var usersGroupedByFirstLetters = users.GroupBy(user => user.Name.Substring(0, 2));
+            foreach (var group in usersGroupedByFirstLetters)
+            {
+                Console.WriteLine("Users starting with " + group.Key + ":");
+                foreach (var user in group)
+                    Console.WriteLine("* " + user.Name);
+            }
+
+
+
+
+
+        }
+        public class User
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public string Country { get; set; }
         }
     }
 
