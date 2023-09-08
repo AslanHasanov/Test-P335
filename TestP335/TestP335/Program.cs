591a147e54e25ebdc0fd94c1dd36c722d79a37a7
Aslan Hasan <aslanrh@code.edu.az>
2023-09-08 14:08:56 +0400

    Lab 7

--- TestP335/TestP335/Program.cs
+++ TestP335/TestP335/Program.cs
@@ -2,65 +2,101 @@
 using System;
 using System.Net.Sockets;
 using System.Threading.Channels;
+using System.Reflection;
 
 
 namespace TestP335
 {
-    public class ShortEmailException : Exception
-    {
-        public ShortEmailException(string message) : base(message) { }
-    }
-
-    public class NotEmailAdressException : Exception
-    {
-        public NotEmailAdressException(string message) : base(message) { }
-    }
-
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            var users = new List<User>()
-            {
-                new User { Name = "John Doe", Age = 42, Country = "USA" },
-                new User { Name = "Jane Doe", Age = 38, Country = "USA" },
-                new User { Name = "Joe Doe", Age = 19, Country = "Germany" },
-                new User { Name = "Jenna Doe", Age = 19, Country = "Germany" },
-                new User { Name = "James Doe", Age = 8, Country = "USA" },
-            };
 
+            // value type
+            int sayi = 42;
 
-            var usersGroupedByCountry = users.GroupBy(user => user.Country);
-
-            foreach (var group in usersGroupedByCountry)
-            {
-                Console.WriteLine($"Users from {group.Key} :");
-
-                foreach (var user in group) Console.WriteLine("* " + user.Name);
-            }
-
-            var usersGroupedByFirstLetters = users.GroupBy(user => user.Name.Substring(0, 2));
-            foreach (var group in usersGroupedByFirstLetters)
-            {
-                Console.WriteLine("Users starting with " + group.Key + ":");
-                foreach (var user in group)
-                    Console.WriteLine("* " + user.Name);
-            }
+            //reference type
+            object boxedSayi = sayi; // Boxing 
 
 
 
 
+            Console.WriteLine(sayi);
+            Console.WriteLine(boxedSayi.GetType());
+
+
+
+
+            //value type
+            int unboxedSayi = (int)boxedSayi;      //Unboxing 
+
+
+
+            Console.WriteLine($"boxedSayi: {boxedSayi}, unboxedSayi: {unboxedSayi}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //SampleClass sample = new SampleClass();
+
+            //// Reflection kullanarak tür bilgisini alalım
+            //Type type = sample.GetType();
+
+            //// Alanları ve değerlerini alma
+            //Console.WriteLine("Alanlar:");
+
+            //FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            //foreach (var field in fields)
+            //{
+            //    Console.WriteLine($"{field.Name}: {field.GetValue(sample)}");
+            //}
+
+            //// Metotları çağırma
+            //Console.WriteLine("\nMetotlar:");
+            //MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            //foreach (var method in methods)
+            //{
+            //    Console.WriteLine(method.Name);
+            //}
 
         }
-        public class User
-        {
-            public string Name { get; set; }
+        //public class SampleClass
+        //{
+        //    public int PublicField = 42;
+        //    private string privateField = "Merhaba, Dünya!";
 
-            public int Age { get; set; }
+        //    public void PublicMethod()
+        //    {
+        //        Console.WriteLine("PublicMethod çağrıldı.");
+        //    }
 
-            public string Country { get; set; }
-        }
+        //    private void PrivateMethod()
+        //    {
+        //        Console.WriteLine("PrivateMethod çağrıldı.");
+        //    }
+        //}
     }
 
 }
